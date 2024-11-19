ec7606067fc1df2664014ace7d016e3af6d8090a
Sirawit Chanaburanasak <punpunsirawit@gmail.com>
2024-11-19 21:04:49 +0700

    feat: balance service

--- internal/domain/service/balance.go
+++ internal/domain/service/balance.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"math/big"
+	"time"
 
 	"github.com/punchanabu/portfolio-tracker/internal/domain/entity"
 	"github.com/punchanabu/portfolio-tracker/internal/domain/repository"
@@ -20,10 +22,39 @@ func NewBalanceService(walletRepo repository.WalletRepository, transactionRepo r
 	}
 }
 
-func (s *BalanceService) GetWalletBalance(ctx context.Context, wallet entity.Wallet) (vo.Money, error) {
-	return vo.Money{}, nil
+func (s *BalanceService) CalculateWalletBalance(ctx context.Context, wallet entity.Wallet) (vo.Money, error) {
+	transactions, err := s.transactionRepo.GetByWalletID(ctx, wallet.ID)
+	if err != nil {
+		return vo.Money{}, err
+	}
+
+	balance := vo.NewMoney(big.NewInt(0), wallet.Balance.Currency)
+
+	for _, tx := range transactions {
+
+		if tx.Status != entity.TransactionStatusSuccess {
+			continue
+		}
+		if tx.From == wallet.Address {
+			balance.Sub(tx.Amount)
+		}
+
+		if tx.To == wallet.Address {
+			balance.Add(tx.Amount)
+		}
+	}
+
+	return balance, nil
 }
 
-func (s* BalanceService) UpdateWalletBalance(ctx context.Context, wallet entity.Wallet) error {
-	return nil
+func (s *BalanceService) UpdateWalletBalance(ctx context.Context, wallet entity.Wallet) error {
+	balance, err := s.CalculateWalletBalance(ctx, wallet)
+	if err != nil {
+		return err
+	}
+
+	wallet.Balance = balance
+	wallet.UpdatedAt = time.Now()
+
+	return s.walletRepo.Update(ctx, &wallet)
 }
